14a421872c8798b4e4ec6c05515ad4b4d5251965
lnm011223 <2969827073@qq.com>
2021-10-09 19:47:28 +0800

    添加刷新功能

--- app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.kt
+++ app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.kt
@@ -8,7 +8,7 @@ class SunnyWeatherApplication : Application() {
 
     companion object {
 
-        const val TOKEN = "BNBRvBveaD2VfHVI" // 填入你申请到的令牌值
+        const val TOKEN = "XZRIod3lJs2JmtAB" // 填入你申请到的令牌值
 
         @SuppressLint("StaticFieldLeak")
         lateinit var context: Context
